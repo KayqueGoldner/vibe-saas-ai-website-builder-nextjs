0bd95cad42d685730f3139ee0164ee5fbd82affd
Kayque Goldner <kayquegoldner@gmail.com>
2025-06-26 22:10:19 -0300

    setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:tailwindcss/recommended",
+    "prettier",
+  ),
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "tailwindcss/no-custom-classname": "off",
+      "tailwindcss/classnames-order": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
