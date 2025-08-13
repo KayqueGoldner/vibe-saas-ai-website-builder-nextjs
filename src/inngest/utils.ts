cbdd2299b4ec9e5728ce1e1fff03559d035ebd51
Kayque Goldner <kayquegoldner@gmail.com>
2025-08-12 23:29:57 -0300

    agent memory and output parsing

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,5 +1,9 @@
 import { Sandbox } from "@e2b/code-interpreter";
-import { AgentResult, TextMessage } from "@inngest/agent-kit";
+import {
+  type AgentResult,
+  type Message,
+  type TextMessage,
+} from "@inngest/agent-kit";
 
 export async function getSandbox(sandboxId: string) {
   const sandbox = await Sandbox.connect(sandboxId);
@@ -22,3 +26,17 @@ export function lastAssistantTextMessageContent(result: AgentResult) {
       : message.content.map((c) => c.text).join("")
     : undefined;
 }
+
+export const parseAgentOutput = (value: Message[]) => {
+  const output = value[0];
+
+  if (output.type !== "text") {
+    return "Fragment";
+  }
+
+  if (Array.isArray(output.content)) {
+    return output.content.map((txt) => txt).join("");
+  } else {
+    return output.content;
+  }
+};
